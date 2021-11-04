e67fde6f27d8f0d72975520c618707f08451dff8
Exodarion <ExoEmail>
2021-11-04 14:32:17 +0100

    Finished assignment 6, still need to add implementation for proper uppercase management though.

--- Opdrachten/MiniOpdracht.java
+++ Opdrachten/MiniOpdracht.java
@@ -1,21 +1,23 @@
 package Working;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class MiniOpdracht 
 {
 	public static int autoID = 0; //Increments the Car ID's
 	public static Random random = new Random(); //Cache a reference to Random
+	public static Sorting sorting = new Sorting(); //Cache a reference to Sorting
 	public static void main(String[] args) 
 	{	
 		MiniOpdracht MiniOpdracht = new MiniOpdracht();
-		MiniOpdracht.Opdracht1();
-		MiniOpdracht.Opdracht2();
-		MiniOpdracht.Opdracht3();
-		MiniOpdracht.Opdracht4(random);
-		MiniOpdracht.Opdracht5(5);
+//		MiniOpdracht.Opdracht1();
+//		MiniOpdracht.Opdracht2();
+//		MiniOpdracht.Opdracht3();
+//		MiniOpdracht.Opdracht4(random);
+//		MiniOpdracht.Opdracht5(5);
+		MiniOpdracht.Opdracht6();
 	}	
 	
 	String Opdracht1 ()
@@ -58,7 +60,6 @@ public class MiniOpdracht
 		System.out.println("Original array content " + Arrays.toString(randomArray)); 
 		
 		//Print newly sorted arrays by value, by sending a copy of the original
-		Sorting sorting = new Sorting();
 		System.out.println("Array content from highest " + Arrays.toString(sorting.SortArrayFromHighest(Arrays.copyOf(randomArray, randomArray.length))));
 		System.out.println("Array content from lowest " + Arrays.toString(sorting.SortArrayFromLowest(Arrays.copyOf(randomArray, randomArray.length))));		
 		System.out.println("Original Array content should still be the same though " + Arrays.toString(randomArray)); 
@@ -74,6 +75,16 @@ public class MiniOpdracht
 		Monteur monteur = new Monteur("Piet", 45);		
 		for(int i = 0; i < iterations; i++)
 			System.out.println("Totale Kosten = " + monteur.GetRepareerKosten(new Auto()));
+	}
+	
+	void Opdracht6 ()
+	{
+		Scanner scanner = new Scanner(System.in);
+		char[] input = scanner.nextLine().toCharArray(); // convert entire line to char array first
+		
+		System.out.println("Original data = " + Arrays.toString(input));
+		System.out.println("Sorted Alphabetical = " + Arrays.toString(sorting.SortArrayFromLowest(Arrays.copyOf(input, input.length))));
+		System.out.println("Sorted anti-Alphabetical = " + Arrays.toString(sorting.SortArrayFromHighest(Arrays.copyOf(input, input.length))));
 	}
 }
 
--- Opdrachten/Sorting.java
+++ Opdrachten/Sorting.java
@@ -3,6 +3,29 @@ package Working;
 //Holds the sorting methods
 public class Sorting 
 {	
+	//Sort the given array of chars from highest to lowest 
+	char[] SortArrayFromHighest(char[] currentArray) 
+	{
+		char[] tempArray = new char[currentArray.length]; 
+		for(int i = 0; i < tempArray.length; i++)
+		{
+			char highest = 0;
+			int index = 0;
+			// Find the highest value for each iteration, then add to the tempList
+			for(int j = 0; j < currentArray.length; j++)
+			{
+				if(currentArray[j] > highest) 
+				{
+					highest = currentArray[j];
+					index = j;
+				}
+			}
+			currentArray[index] = 0; //nullify the current highest value for next iteration
+			tempArray[i] = highest; //set current index to the highest		
+		}
+		return tempArray;
+	}
+	
 	// Sort the given array of ints from highest to lowest 
 	int[] SortArrayFromHighest(int[] currentArray) 
 	{
@@ -22,6 +45,29 @@ public class Sorting
 			}
 			currentArray[index] = 0; //nullify the current highest value for next iteration
 			tempArray[i] = highest; //set current index to the highest		
+		}
+		return tempArray;
+	}
+	
+	// Sort the given array of chars from lowest to highest 
+	char[] SortArrayFromLowest(char[] currentArray) 
+	{		
+		char[] tempArray = new char[currentArray.length];
+		// Find the lowest value for each iteration, then add to the tempList
+		for(int i = 0; i < tempArray.length; i++)
+		{
+			char lowest = Character.MAX_VALUE; // init to max value
+			int index = 0;
+			for(int j = 0; j < currentArray.length; j++)
+			{
+				if(currentArray[j] < lowest) 
+				{
+					lowest = currentArray[j];
+					index = j;
+				}
+			}
+			currentArray[index] = Character.MAX_VALUE; //nullify the current lowest value for next iteration
+			tempArray[i] = lowest; //set current index to the lowest			
 		}
 		return tempArray;
 	}
